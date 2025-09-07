4b4fdcdd74de01620fd08a760a8e58620c7940b7
rahul pareek <itsrahul615@gmail.com>
2025-09-07 11:02:00 +0530

    In demo.java practiced compile time(checked) exception

--- excep_hand/src/excep_hand/Demo.java
+++ excep_hand/src/excep_hand/Demo.java
@@ -1,20 +1,48 @@
 package excep_hand;
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Demo {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) throws IOException{
-		File f1 = new File("file.txt");
-		
-		// Now read content back using BufferedReader
-        FileReader fr = new FileReader(f1);
-        BufferedReader br = new BufferedReader(fr);
-
-        String line;
-        while((line = br.readLine())!= null) {
-        	System.out.println(line);
+        // FileNotFoundException
+        try {
+            FileReader f3 = new FileReader("rahul.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException: " + e);
         }
-        br.close();
-	}
 
+        // ClassNotFoundException
+        try {
+            Class.forName("com.unknown.Class");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException: " + e);
+        }
+
+        // SQLException
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "root", "pass");
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e);
+        }
+
+        // NoSuchMethodException
+        try {
+            Demo.class.getMethod("unknowMethod");
+        } catch (NoSuchMethodException e) {
+            System.out.println("NoSuchMethodException: " + e);
+        }
+        
+        // MalformedURLException
+        try {
+        	URL url = new URL("htp://wrongurl");
+        } catch(MalformedURLException e) {
+        	System.out.println(e);
+        }
+        
+    }
 }
